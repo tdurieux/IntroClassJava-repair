e7ac4aa4f607912e8b7ae9305cac9efe6a26fe15
tdurieux <durieuxthomas@hotmail.com>
2018-03-13 14:22:38 +0100

    fix script

--- .travis-run.sh
+++ .travis-run.sh
@@ -1,11 +1,12 @@
 cd IntroClassJava/dataset;
 cd $BUG;
-for D in `find . -type d`
-do
-	echo ${D};
-    cd ${D};
-    pwd
-    mvn test || true;
-	mvn fr.inria.gforge.spirals:repair-maven-plugin:nopol;
-    cd ..;
+for D in *; do
+	if [ -d "${D}" ]; then
+		echo "${D}"
+		cd ${D};
+		pwd
+		mvn test || true;
+		mvn fr.inria.gforge.spirals:repair-maven-plugin:nopol;
+		cd ..;
+	fi
 done
