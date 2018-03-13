990b67226c5d966aa44e2c76960a2bda108cbbb3
tdurieux <durieuxthomas@hotmail.com>
2018-03-13 14:12:59 +0100

    test script

--- .travis-run.sh
+++ .travis-run.sh
@@ -1,9 +1,10 @@
-cd IntroClassJava/dataset
-cd $BUG
+cd IntroClassJava/dataset;
+cd $BUG;
 for D in `find . -type d`
 do
-    cd $D
-    mvn test || true
-	mvn fr.inria.gforge.spirals:repair-maven-plugin:nopol
-    cd ..
+    cd $D;
+    pwd
+    mvn test || true;
+	mvn fr.inria.gforge.spirals:repair-maven-plugin:nopol;
+    cd ..;
 done
